fbe1b584876c680039ac537ab650687a4a6f8709
gabriel <gaamdal557@gmail.com>
2023-03-28 14:49:06 -0300

    removendo mocks part 2

--- API-rest/Service/Implementation/PersonServiceImpl.cs
+++ API-rest/Service/Implementation/PersonServiceImpl.cs
@@ -1,5 +1,6 @@
 ﻿using API_rest.Model;
 using API_rest.Model.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -15,36 +16,76 @@ namespace API_rest.Service.Implementation
             _context = context;
         }
 
-        public Person Create(Person person)
-        {
-            return person;
-        }
-   
-        public void Delete(long id)
-        {
-        }
-
         public List<Person> FindAll()
         {
             return _context.Persons.ToList();
         }
 
-     
+
         public Person FindByID(long id)
         {
-            return new Person
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
             {
-                Id = 1,
-                FirstName = "Leandro",
-                LastName = "Costa",
-                Address = "Uberlandia - Minas Gerais - Brasil",
-                Gender = "Male"
-            };
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return person;
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
             return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }

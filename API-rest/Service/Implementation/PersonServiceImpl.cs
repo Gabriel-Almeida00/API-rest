948860fc53874d536a1ad71156127ce12a110510
gabriel <gaamdal557@gmail.com>
2023-03-28 14:34:04 -0300

    removendo mocks part 1

--- API-rest/Service/Implementation/PersonServiceImpl.cs
+++ API-rest/Service/Implementation/PersonServiceImpl.cs
@@ -1,34 +1,32 @@
 ﻿using API_rest.Model;
+using API_rest.Model.Context;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace API_rest.Service.Implementation
 {
     public class PersonServiceImpl : IPersonService
     {
-        private volatile int count;
+        private MySqlContext _context;
 
+        public PersonServiceImpl(MySqlContext context)
+        {
+            _context = context;
+        }
 
         public Person Create(Person person)
         {
             return person;
         }
-
    
         public void Delete(long id)
         {
         }
 
- 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-            for (int i = 0; i < 8; i++)
-            {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            }
-            return persons;
+            return _context.Persons.ToList();
         }
 
      
@@ -36,7 +34,7 @@ namespace API_rest.Service.Implementation
         {
             return new Person
             {
-                Id = IncrementAndGet(),
+                Id = 1,
                 FirstName = "Leandro",
                 LastName = "Costa",
                 Address = "Uberlandia - Minas Gerais - Brasil",
@@ -47,23 +45,6 @@ namespace API_rest.Service.Implementation
         public Person Update(Person person)
         {
             return person;
-        }
-
-        private Person MockPerson(int i)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name" + i,
-                LastName = "Person LastName" + i,
-                Address = "Some Address" + i,
-                Gender = "Male"
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
         }
     }
 }

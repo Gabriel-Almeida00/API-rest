5278c51e767e33a94fd031ab3d79a757f9d7d26e
gabriel <gaamdal557@gmail.com>
2023-03-28 13:24:19 -0300

    service e implementation

---
+++ API-rest/Service/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using API_rest.Model;
+using System.Collections.Generic;
+
+namespace API_rest.Service
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}

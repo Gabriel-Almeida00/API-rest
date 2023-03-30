5b8c3789e9a41d65a3f5bf9dd63b4833ef85b79c
gabriel <gaamdal557@gmail.com>
2023-03-30 16:28:04 -0300

    implementando VO part 1

---
+++ API-rest/Data/Converter/Contratc/IParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace API_rest.Data.Converter.Contratc
+{
+    public interface IParser<O, D>
+    {
+        D Parse(O origin);
+        List<D> Parse(List<O> origin);
+    }
+}

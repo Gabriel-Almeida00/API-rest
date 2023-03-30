3c7bd3115c7955a8acf4a0edaa462cde2155d3d3
gabriel <gaamdal557@gmail.com>
2023-03-30 16:45:37 -0300

    bookVO

---
+++ API-rest/Data/Converter/Implementation/BookConverter.cs
@@ -0,0 +1,49 @@
+﻿using API_rest.Data.Converter.Contratc;
+using API_rest.Data.VO;
+using API_rest.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API_rest.Data.Converter.Implementation
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {   
+        public Book Parse(BookVO origin)
+        {
+            if (origin is null) return null;
+            return new Book
+            {
+                Id = origin.Id,
+                author = origin.author,
+                launchDate = origin.launchDate,
+                price = origin.price,
+                title = origin.title
+            };
+        }
+
+        public BookVO Parse(Book origin)
+        {
+            if (origin is null) return null;
+            return new BookVO
+            {
+                Id = origin.Id,
+                author = origin.author,
+                launchDate = origin.launchDate,
+                price = origin.price,
+                title = origin.title
+            };
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin is null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin is null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
---
+++ API-rest/Data/VO/BookVO.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace API_rest.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+
+        public string author { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime launchDate { get; set; }
+
+        public Decimal price { get; set; }
+
+        public string title { get; set; }
+    }
+}
